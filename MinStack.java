3d5fe807568db3c4fdf6fe93d707624a340acacd
Nhan T Thai <43976041+ntthai@users.noreply.github.com>
2019-03-07 20:56:09 -0600

    Update MinStack.java

--- MinStack.java
+++ MinStack.java
@@ -1,5 +1,6 @@
 package StacknQueue;
-
+/* How would you design a stack which, in addition to push and pop, has a function min which returns the minimum 
+element? Push, pop and min should all operate in 0(1) time.*/
 public class MinStack {
 	class Node {
 		int val;
