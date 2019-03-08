1bf5d33b0bb3b6cbeac9f71aa0f9f47d6c864ec2
Nhan T Thai <43976041+ntthai@users.noreply.github.com>
2019-03-07 20:57:14 -0600

    Update QueueViaStack.java

--- QueueViaStack.java
+++ QueueViaStack.java
@@ -1,5 +1,8 @@
 package StacknQueue;
 import java.util.*;
+/* Write a program to sort a stack such that the smallest items are on the top. You can use an additional 
+temporary stack, but you may not copy the elements into any other data structure (such as an array). The 
+stack supports the following operations: push, pop, peek, and isEmpty*/
 public class QueueViaStack {
 	static class Queue { 
 		static Stack<Integer> s1 = new Stack<Integer>();  
